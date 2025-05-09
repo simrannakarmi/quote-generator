b65d728c90991fff1d7d456cec1a56cc3edd3e9e
simrannakarmi <nakarmisimran@gmail.com>
2025-05-09 09:37:54 +0545

    Added quotes ina separate file

---
+++ quotes.js
@@ -0,0 +1,140 @@
+const quotes = {
+  life: [
+    {
+      text: "You have to believe in yourself when no one else does.",
+      author: "Serena Williams",
+    },
+    {
+      text: "When you have a dream, you’ve got to grab it and never let go.",
+      author: "Carol Burnett",
+    },
+    {
+      text: "The most important thing is to enjoy your life—to be happy—it’s all that matters.",
+      author: "Steve Jobs",
+    },
+    {
+      text: "Spread love everywhere you go. Let no one ever come without leaving happier.",
+      author: "Mother Teresa",
+    },
+    {
+      text: "Be yourself; everyone else is already taken.",
+      author: "Oscar Wilde",
+    },
+    {
+      text: "Some people want it to happen, some wish it would happen, others make it happen.",
+      author: "Michael Jordan",
+    },
+    {
+      text: "You can’t help what you feel, but you can help how you behave.",
+      author: "Margaret Atwood",
+    },
+    {
+      text: "We become what we think about most of the time.",
+      author: "Earl Nightingale",
+    },
+  ],
+  art: [
+    {
+      text: "Art is everywhere, except it has to pass through a creative mind.",
+      author: "L. Nevelson",
+    },
+    {
+      text: "Art is literacy of the heart.",
+      author: "Elliot Eisner",
+    },
+    {
+      text: "Art doesn't have to be pretty. It has to be meaningful.",
+      author: "Duane Hanson",
+    },
+    {
+      text: "Art does not imitate, but interpret.",
+      author: "Giuseppe Mazzini",
+    },
+    {
+      text: "Art is the only way to run away without leaving home.",
+      author: "Twyla Tharp",
+    },
+    {
+      text: "Art is the most intense mode of individualism that the world has known.",
+      author: "Oscar Wilde",
+    },
+  ],
+  spirituality: [
+    {
+      text: "The most wasted of all days is one without laughter.",
+      author: "Nicolas Chamfort",
+    },
+    {
+      text: "Believe in your infinite potential. Your only limitations are those you set upon yourself.",
+      author: "Roy T. Bennett",
+    },
+    {
+      text: "We are not human beings having a spiritual experience. We are spiritual beings having a human experience.",
+      author: "Pierre Teilhard de Chardin",
+    },
+    {
+      text: "What you are is God's gift to you, what you become is your gift to God.",
+      author: "Hans Urs von Balthasar",
+    },
+    {
+      text: "A quiet conscience makes one strong!",
+      author: "Anne Frank",
+    },
+    {
+      text: "When you connect to the silence within you, that is when you can make sense of the disturbance going on around you.",
+      author: "Stephen Richards",
+    },
+  ],
+  music: [
+    {
+      text: "Music is the strongest form of magic.",
+      author: "Marilyn Manson",
+    },
+    {
+      text: "Music in the soul can be heard by the universe.",
+      author: "Lao Tzu",
+    },
+    {
+      text: "Music is well said to be the speech of angels.",
+      author: "Thomas Carlyle",
+    },
+    {
+      text: "Music is very spiritual, it has the power to bring people together.",
+      author: "Edgar Winter",
+    },
+    {
+      text: "Without music, life would be a mistake.",
+      author: "Friedrich Nietzsche",
+    },
+    {
+      text: "Where words fail, music speaks.",
+      author: "Hans Christian Andersen",
+    },
+  ],
+  motivational: [
+    {
+      text: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+      author: "Roy T. Bennett",
+    },
+    {
+      text: "Happiness is not the absence of problems, it's the ability to deal with them.",
+      author: "Steve Maraboli",
+    },
+    {
+      text: "Do what is right, not what is easy nor what is popular.",
+      author: "Roy T. Bennett",
+    },
+    {
+      text: "The only way of discovering the limits of the possible is to venture a little way past them into the impossible.",
+      author: "Arthur C. Clarke",
+    },
+    {
+      text: "Cry. Forgive. Learn. Move on. Let your tears water the seeds of your future happiness.",
+      author: "Steve Maraboli",
+    },
+    {
+      text: "Do not fear failure but rather fear not trying.",
+      author: "Roy T. Bennett",
+    },
+  ],
+};
